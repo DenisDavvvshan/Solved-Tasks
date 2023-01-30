8c1468b1fed922a32bed9da368772836b15505f2
DenisDavvvshan <110508362+DenisDavvvshan@users.noreply.github.com>
2023-01-30 22:29:40 +0300

    Delete Task3.js

--- Task3.js
+++
@@ -1,8 +0,0 @@
-// Функция, которая создает объект компании и возвращает его.
-
-const make = (companyName, obj) => {
-    let defaultR = { state: 'moderating', createdAt: Date.now() };
-    defaultR.name = companyName;
-    const result = { ...defaultR, ...obj };
-    return result;
-  };
